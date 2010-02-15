ad702531850a81b17c97578fb1d02456b58da4ee
Your Name <dan@telent.net>
2010-02-15 12:25:42 +0000

    start specifying behaviours related to the chart of accounts

---
+++ spec/chart_spec.rb
@@ -0,0 +1,8 @@
+require 'support/db'
+require 'pacioli'
+
+describe "Chart of Accounts" do
+  it "should populate default database with outline chart of accounts"
+  it "should generate balance sheet report"
+  it "should generate p+l report and balancing tx"
+end
